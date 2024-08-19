8854a10008f04889c5e5c9e6905d159e21ee44c8
cs_sido <csaba.sidor@gmail.com>
2024-08-19 10:30:08 +0200

    Update db_connector.py

--- source_codes/modules/db_connector.py
+++ source_codes/modules/db_connector.py
@@ -3,6 +3,6 @@ from geoalchemy2 import Geometry, WKTElement
 
 from sqlalchemy import create_engine, MetaData, Table, text
 import pandas.io.sql as psql
-engine = create_engine('postgresql+psycopg2://postgres:17021987@localhost:5432/dbis')
+engine = create_engine('postgresql+psycopg2://USER:PSWF@HOST:PORT/DB')
 
 
